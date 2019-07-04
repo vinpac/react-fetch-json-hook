1e0681dc0f901aa182a2da88bcc751ffdf30de98
Vinicius Pacheco Furtado <vin175pacheco@gmail.com>
2019-07-04 16:49:14 -0300

    Disable json parsing if response has 204 status

--- src/fetch-json.ts
+++ src/fetch-json.ts
@@ -4,5 +4,11 @@ export default function fetchJSON<Payload>(
   uri: string,
   options: RequestInit,
 ): Promise<Payload> {
-  return fetch(uri, options).then(response => response.json())
+  return fetch(uri, options).then(response => {
+    if (response.status === 204) {
+      return null
+    }
+
+    return response.json()
+  })
 }
